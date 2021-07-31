87dfc516631de8c10d3ce70ff7020af3c055bb84
griffin-stewie <panterathefamilyguy@gmail.com>
2021-07-31 12:40:47 +0900

    update to ArgumentParser v0.4.4

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "sample_cal",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-argument-parser", .exact("0.4.3")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .exact("0.4.4")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
